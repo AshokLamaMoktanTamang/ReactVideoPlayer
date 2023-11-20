7be683a1284b8caeea10849aaeddff69cbbe6b8c
Ashok Lama <ashok.lama@innovatetech.co>
2023-11-20 16:06:53 +0545

    [Update] Updated the desig of the button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,26 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { IButton } from 'types/button';
 
 import style from './style.module.scss'
 
 const Button: FC<IButton> = ({ icon, toolTip, onClick }) => {
+    const [tooltipTitle, setTooltipTitle] = useState<string | null>(toolTip || null)
+
     return (
         <button
             className={style.button}
             onClick={onClick}
+            onMouseDown={() => setTooltipTitle(null)}
+            onMouseLeave={() => setTooltipTitle(toolTip || null)}
         >
             {icon}
-            {toolTip}
+            {
+                tooltipTitle &&
+                <span className={style.toolTip}>
+                    {toolTip}
+                </span>
+            }
         </button>
     );
 }
